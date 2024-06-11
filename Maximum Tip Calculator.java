7d9aeeab7126769280f0de45eec6795dfcbcde41
Sarvesh Adithya J <133513238+sarveshadithya17@users.noreply.github.com>
2024-06-11 22:32:31 +0530

    Create Maximum Tip Calculator.java

---
+++ Maximum Tip Calculator.java
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class IntArray {
+    public static int[] input(BufferedReader br, int n) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int n;
+            n = Integer.parseInt(br.readLine());
+
+            int x;
+            x = Integer.parseInt(br.readLine());
+
+            int y;
+            y = Integer.parseInt(br.readLine());
+
+            int[] arr = IntArray.input(br, n);
+
+            int[] brr = IntArray.input(br, n);
+
+            Solution obj = new Solution();
+            long res = obj.maxTip(n, x, y, arr, brr);
+
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static long maxTip(int n, int x, int y, int[] arr, int[] brr) {
+        List<int[]> orders = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            orders.add(new int[]{Math.abs(arr[i] - brr[i]), arr[i], brr[i], i});
+        }
+        
+        orders.sort((a, b) -> b[0] - a[0]);
+
+        long maxTip = 0;
+        int aCount = 0;
+        int bCount = 0;
+
+        for (int[] order : orders) {
+            int tipA = order[1];
+            int tipB = order[2];
+            int index = order[3];
+
+            if ((tipA >= tipB && aCount < x) || bCount >= y) {
+                maxTip += tipA;
+                aCount++;
+            } else {
+                maxTip += tipB;
+                bCount++;
+            }
+        }
+
+        return maxTip;
+    }
+}
